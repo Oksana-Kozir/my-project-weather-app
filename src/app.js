35568234d58d5719c871b24bd6a559815e203af8
Oksana <ksanakozir@gmail.com>
2022-08-13 18:53:52 +0300

    weather icon dont work

--- src/app.js
+++ src/app.js
@@ -37,6 +37,11 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "1aab64290751190d2719fa18bf8fa94d";
